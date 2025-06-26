069fb0cad529c5b80436c07198b4ec49627d728c
ozakikatsuhiko <startrek1984c@gmail.com>
2025-06-26 23:59:00 +0900

    first commit.

--- initialize.py
+++ initialize.py
@@ -120,6 +120,8 @@ def initialize_agent_executor():
     st.session_state.service_doc_chain = utils.create_rag_chain(ct.DB_SERVICE_PATH)
     st.session_state.company_doc_chain = utils.create_rag_chain(ct.DB_COMPANY_PATH)
     st.session_state.rag_chain = utils.create_rag_chain(ct.DB_ALL_PATH)
+    DB_COMPANYALL_PATH = ct.DB_SERVICE_PATH+  ct.DB_COMPANY_PATH
+    st.session_state.company_data_chain = utils.create_rag_chain(ct.DB_COMPANYALL_PATH)
 
     # Web検索用のToolを設定するためのオブジェクトを用意
     search = SerpAPIWrapper()
@@ -148,6 +150,12 @@ def initialize_agent_executor():
             name = ct.SEARCH_WEB_INFO_TOOL_NAME,
             func=search.run,
             description=ct.SEARCH_WEB_INFO_TOOL_DESCRIPTION
+        ),
+        # 自社全般検索用のTool
+        Tool(
+            name = ct.SEARCH_COMPANYALL_INFO_TOOL_NAME,
+            func=utils.run_company_data_chain,
+            description=ct.SEARCH_COMPANYSALL_INFO_TOOL_DESCRIPTION
         )
     ]
 
